fde87ddd3c666740cc6c98ea3edfec3e8cfb1588
alee <alee@aleeui-MacBookPro.local>
2023-01-09 03:52:34 +0900

    feat : home_screen

--- i_am_a_cat/lib/main.dart
+++ i_am_a_cat/lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:i_am_a_cat/screen/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'I am a cat',
       home: HomeScreen(),
     );
   }
 }
-
-
--- i_am_a_cat/lib/screen/home_screen.dart
+++ i_am_a_cat/lib/screen/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('I am a cat'),
+        title: const Text('I am a cat'),
         backgroundColor: Colors.grey[400],
       ),
       body: ListView.separated(
