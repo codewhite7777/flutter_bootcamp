ef0f528931496ff95304af960b2be62949446cf1
alee <alee@aleeui-MacBookPro.local>
2023-02-05 06:58:14 +0900

    chore : class WeatherModel - element add

--- clima/lib/model/weather_model.dart
+++ clima/lib/model/weather_model.dart
@@ -3,9 +3,11 @@ class WeatherModel {
     required this.name,
     required this.main,
     required this.description,
+    required this.temp,
   });
 
   final String name;
   final String main;
   final String description;
+  final double  temp;
 }
